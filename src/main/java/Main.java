e43c14c643955bbc7474f6b5adbbe3bde9c7b0d2
Pablo Henrique da Silva Andrade <pablobreak2017@alu.ufc.br>
2023-09-20 19:12:50 -0300

    update name to Main

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+import View.Login;
+
+public class Main {
+    public static void main(String[] args) {
+        new Login().setVisible(true);
+    }
+}
--- src/main/java/Principal.java
+++
@@ -1,7 +0,0 @@
-import View.Login;
-
-public class Principal {
-    public static void main(String[] args){
-        new Login().setVisible(true);
-    }
-}
